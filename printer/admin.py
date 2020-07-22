f0d7c8f0e3da7fa9d24d3d5ff8c5215d4fd5cc2f
PyGuDev <gubaev1999@gmail.com>
2020-07-22 20:06:34 +0500

    Добавил модели для Чеков и Принтеров

--- printer/admin.py
+++ printer/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Printer, Check
 
-# Register your models here.
+
+@admin.register(Printer)
+class PrinterAdmin(admin.ModelAdmin):
+    list_display = ('name', 'check_type', 'point_id', 'api_key')
+    list_filter = ('check_type', 'point_id')
+
+
+@admin.register(Check)
+class CheckAdmin(admin.ModelAdmin):
+    list_display = ('printer_id', 'type', 'order', 'status')
+    list_filter = ('printer_id', 'type', 'status')
--- printer/models.py
+++ printer/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
-# Create your models here.
+
+CHECK_TYPE  = [
+        ('kitchen', 'kitchen'),
+        ('client', 'client'),
+    ]
+
+
+class Printer(models.Model):
+    name = models.CharField('Название принтера', max_length=30)
+    api_key = models.CharField(max_length=255)
+    check_type = models.CharField('Тип чека', max_length=8, choices=CHECK_TYPE)
+    point_id = models.IntegerField('Точка')
+
+    
+    class Meta:
+        verbose_name = 'Принтер'
+        verbose_name_plural = 'Принтеры'
+    
+
+class Check(models.Model):
+    STATUS = [
+        ('news', 'news'),
+        ('rendered', 'rendered'),
+        ('printed', 'printed'),
+    ]
+
+    printer_id = models.ForeignKey("Printer", on_delete=models.CASCADE)
+    type = models.CharField('Тип чека', max_length=8, choices=CHECK_TYPE)
+    order = JSONField('Информация заказа')
+    status = models.CharField('Статус', max_length=10, choices=STATUS)
+    pdf_file = models.FileField('PDF', upload_to='pdf/')
+
+
+    class Meta:
+        verbose_name = 'Чек'
+        verbose_name_plural = 'Чеки'
