0cc0f8e95a86a621306cbc8ad9cbaedeee0367f9
PyGuDev <gubaev1999@gmail.com>
2020-07-25 21:53:54 +0500

    Добавил сериалайзер Check

--- printer/serializers.py
+++ printer/serializers.py
@@ -3,23 +3,33 @@ from .models import Printer, Check
 
 
 class CheckCreateSerializer(serializers.ModelSerializer):
-    """Создание чеков"""
-    
+    """Сериалайзер чеков и создание их"""
+
     class Meta:
         model = Check
         fields = "__all__"
 
     def create(self, validated_data, check_type):
-        #Поиск модели принтера
+        # Поиск модели принтера
         printer = Printer.objects.get(
             point_id=validated_data.get('point_id', None),
             check_type=check_type)
-        #Создание модели чека
+        # Создание модели чека
         check = Check.objects.create(
-            type= check_type,
+            type=check_type,
             order=validated_data,
             status='new',
             pdf_file=None,
             printer_id=printer
         )
         return check
+
+
+class CheckListSerializer(serializers.ModelSerializer):
+    """Сериалайзер списка чеков"""
+    class Meta:
+        model = Check
+        fields = ['pk']
+
+
+
