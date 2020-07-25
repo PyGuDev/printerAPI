1a2010cc631f1c14b22e03876dc1133c340172d9
PyGuDev <gubaev1999@gmail.com>
2020-07-25 21:56:25 +0500

    Добавил urls пути

--- printer/urls.py
+++ printer/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url, include
-from .views import CheckCreateView
+from .views import CheckCreateView, AvailableCheckView, CheckView
 
 
 urlpatterns = [
     url(r'^create_checks/', CheckCreateView.as_view()),
+    url(r'^new_checks/', AvailableCheckView.as_view()),
+    url(r'^check/', CheckView.as_view()),
 ]
 urlpatterns += [
     url(r'^django-rq/', include('django_rq.urls')),
